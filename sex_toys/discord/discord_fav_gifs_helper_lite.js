dc5dc41b9f5c292b41bbc95928846f82991cd05b
Mr.Kleiner <megaadrenaline1055@gmail.com>
2021-06-20 01:20:10 +0300

    added new tool
    discord gif link saver first release

---
+++ sex_toys/discord/discord_fav_gifs_helper_lite.js
@@ -0,0 +1,17 @@
+$('.fuckingshit').remove();
+$('.layerContainer-yqaFcK section.positionContainer-DEuh7X').css({height: '3000px'});
+$('body').append('<style class="tmp_bdsm">.tmp_hide{display: none !important}</style>');
+$('.sidebar-2K8pFh .scroller-RmtA4e *').addClass('tmp_hide');
+setTimeout(function(){
+	$('.sidebar-2K8pFh .scroller-RmtA4e').append('<textarea class="fuckingshit">click me, press ctrl + a and then ctrl + c</textarea>');
+	$('.content-3YMskv .result-3w1ZcL').each(function(){
+		$('.fuckingshit').val($('.fuckingshit').val() + '\n' + $(this).find('.gif-1TcNIB').attr('src'));
+	});
+	$('.sidebar-2K8pFh .scroller-RmtA4e').append('<button class="dick_sounding">Click me when youre done</button>');
+	$('.dick_sounding').click(function(e){
+		$('.sidebar-2K8pFh .scroller-RmtA4e *').removeClass('tmp_hide');
+		$('.fuckingshit').remove();
+		$('.dick_sounding').remove();
+		$('.tmp_bdsm').remove();
+	});
+}, 3000);
