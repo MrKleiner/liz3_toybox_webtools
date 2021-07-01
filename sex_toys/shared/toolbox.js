065ceb93a02b57afe38d6ee25789e545a77e490e
Mr.Kleiner <megaadrenaline1055@gmail.com>
2021-07-01 06:44:43 +0300

    updated toolbox
    added copypasted functions for cookie manipulation

--- sex_toys/shared/toolbox.js
+++ sex_toys/shared/toolbox.js
@@ -187,3 +187,43 @@ function liz3_rndwave(length, method) {
 }
 
 // console.log(makeid(16));
+
+
+
+
+
+// set cookies. from https://www.w3schools.com/js/js_cookies.asp
+
+function liz3_setCookie(cname, cvalue, exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function liz3_getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function liz3_checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    alert("Welcome again " + user);
+  } else {
+    user = prompt("Please enter your name:", "");
+    if (user != "" && user != null) {
+      setCookie("username", user, 365);
+    }
+  }
+}
