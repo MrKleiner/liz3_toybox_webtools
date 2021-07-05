80312854e0b3f6d0a7d7f6d4847dbed21bee74f5
Mr.Kleiner <megaadrenaline1055@gmail.com>
2021-07-05 05:16:05 +0300

    updated toolbox
    added rgb to hex convertor

--- sex_toys/shared/toolbox.js
+++ sex_toys/shared/toolbox.js
@@ -287,3 +287,13 @@ function liz3_copytext(l3text)
 	document.execCommand("copy");
 	$temp.remove();
 }
+
+
+// convert rgb to hex
+function liz3_rgb2hex(rgb) {
+    rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+        return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+    return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
